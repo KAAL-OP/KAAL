b836fb387783012f51776072d4950c901acfd69e
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-07-20 22:18:21 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-os.system("git clone https://github.com/VENOM-USERBOT/DARK-VENOM venom")
-os.chdir("venom")
+os.system("git clone https://github.com/kaal-op/KAAL cd KAAL")
+os.chdir("kaal")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
