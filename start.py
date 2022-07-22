0aee8dba16d339b11c8abf68399590de165e9bc5
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-07-22 18:21:13 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,10 +3,10 @@ import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
-os.system("git clone https://gitHub.com/kaal-op/KAAL && cd KAAL && python3 -m userbot")
-os.chdir("KAAL")
+os.system("git clone https://gitHub.com/kaal-op/KAAL && cd KAAL && python3 -m KAALX")
+os.chdir("KAALX")
 process = subprocess.Popen(
-        ["python3", "-m", "userbot"],
+        ["python3", "-m", "KAALX"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,)
 out, er = process.communicate()
