f73d9412cd9c8dc020cb1965bcbef08d6df8b128
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-07-22 18:38:53 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,4 @@
-import os 
-import subprocess
-from logging import DEBUG, INFO, basicConfig, getLogger, warning
-basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
-LOGS = getLogger("Helper")
-os.system("git clone https://gitHub.com/kaal-op/KAAL && cd KAAL && python3 -m KAALX")
-os.chdir("KAALX")
-process = subprocess.Popen(
-        ["python3", "-m", "KAALX"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,)
-out, er = process.communicate()
-if er:
-    LOGS.warning(er.decode())
-print("::::::::::::::")
-if out:
-    LOGS.info(out.decode())
+from os import system
+system("git clone https://gitHub.com/kaal-op/KAAL && cd KAAL && python3 -m KAALX")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Murat_30_God")
+#
